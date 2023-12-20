859ee9fb30d612c7b594e81c9eb60b3dc617d06b
Josie <tinaweizq@outlook.com>
2023-12-19 20:32:32 -0800

    Refactor CreateOrder component and fix Order component key issue

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -2,6 +2,11 @@ import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
 import { useSelector } from 'react-redux';
+import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
+import EmptyCart from '../cart/EmptyCart';
+import store from '../../store';
+import { formatCurrency } from '../../utils/helpers';
+import { useState } from 'react';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -9,39 +14,23 @@ const isValidPhone = (str) =>
     str,
   );
 
-const fakeCart = [
-  {
-    pizzaId: 12,
-    name: 'Mediterranean',
-    quantity: 2,
-    unitPrice: 16,
-    totalPrice: 32,
-  },
-  {
-    pizzaId: 6,
-    name: 'Vegetale',
-    quantity: 1,
-    unitPrice: 13,
-    totalPrice: 13,
-  },
-  {
-    pizzaId: 11,
-    name: 'Spinach and Mushroom',
-    quantity: 1,
-    unitPrice: 15,
-    totalPrice: 15,
-  },
-];
-
 function CreateOrder() {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
   // useActionData() is a React hook that returns the action data.
   const formErrors = useActionData();
-  // const [withPriority, setWithPriority] = useState(false);
-  const cart = fakeCart;
+  const [withPriority, setWithPriority] = useState(false);
+  
    // read the username from the Redux store
    const username = useSelector((state)=>state.user.username);
+
+   const cart = useSelector(getCart);
+
+   const totalCartPrice = useSelector(getTotalCartPrice);
+   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
+   const totalPrice = totalCartPrice + priorityPrice;
+
+   if(!cart.length){return <EmptyCart />}
 
   return (
     <div className='px-4 py-6'>
@@ -75,20 +64,19 @@ function CreateOrder() {
             type="checkbox"
             name="priority"
             id="priority"
-            // value={withPriority}
-            // onChange={(e) => setWithPriority(e.target.checked)}
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label className='font-medium' htmlFor="priority">Want to yo give your order priority?</label>
         </div>
 
         <div >
-          {/* JSON.stringify is a JavaScript method used to convert a JavaScript object or value into a JSON string */}
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
           <Button
             disabled={isSubmitting}
             type='primary'
           >
-            {isSubmitting ? 'Placing order...' : 'Order now'}
+            {isSubmitting ? 'Placing order...' : `Order now from ${formatCurrency(totalPrice)}` }
           </Button>
         </div>
       </Form>
@@ -96,6 +84,7 @@ function CreateOrder() {
   );
 }
 
+// The action() function is called when the form is submitted.
 export async function action({ request }) {
   const formData = await request.formData();
   // convert FormData to regular object
@@ -103,7 +92,7 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === 'on',
+    priority: data.priority === 'true',
   };
 
   const errors = {};
@@ -116,6 +105,10 @@ export async function action({ request }) {
     return errors;
   }
   const newOrder = await createOrder(order);
+
+  // clear the cart by dispatching the clearCart action
+  store.dispatch(clearCart());
+
   // redirect to the new order page
   return redirect(`/order/${newOrder.id}`);
 }
